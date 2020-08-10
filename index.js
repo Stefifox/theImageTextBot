455df6eedb98c0a372e8cf1bc1a11717b20a931e
Stefano <30950154+Stefifox@users.noreply.github.com>
2020-08-10 22:01:14 +0200

    Updated Index.js - Added Font File

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const Telegrambot = require("node-telegram-bot-api")
 const png = require('text2png')
-//const locale = require("./locale.json")
+const locale = require("./locales.json")
 const set = require("./settings.json")
 
 var Telegram = new Telegrambot(set.apiKey, {
     polling: true
 })
 
-var imageSettings = {
-    color: 'black',
-    backgroundColor: 'white',
-    textAlign: 'center',
-    lineSpacing: 10,
-    padding: 20
-}
+// ImageSettings
+var imageSettings = [{
+        color: 'black',
+        backgroundColor: 'white',
+        textAlign: 'center',
+        lineSpacing: 10,
+        padding: 20,
+        font: '40px Lemonada',
+        localFontPath: 'resources/Lemonada.ttf',
+        localFontName: 'Lemonada'
+    },
+    {
+        color: 'white',
+        backgroundColor: 'black',
+        textAlign: 'center',
+        lineSpacing: 10,
+        padding: 20,
+        font: '40px Lemonada',
+        localFontPath: 'resources/Lemonada.ttf',
+        localFontName: 'Lemonada'
+    }
+]
 
 //var startKeyboard = 
 
@@ -28,24 +43,43 @@ function getLanguage(msg) {
     })
 }
 
-Telegram.on('message', msg=>{
+Telegram.on('message', msg => {
     console.log(msg)
-    if(msg.text.search("/") !== 0){
-       Telegram.sendPhoto(msg.chat.id, png(msg.text, imageSettings))
+    if (msg.text.search("/") !== 0) {
+        Telegram.sendPhoto(msg.chat.id, png(msg.text, imageSettings[0]))
+    } else {
+        getLanguage(msg).then(lang => {
+            switch (msg.text) {
+                case "/start":
+                    Telegram.sendMessage(msg.chat.id, locale[lang].welcome)
+                    break
+                default:
+                    Telegram.sendMessage(msg.chat.id, locale[lang].null)
+                    break
+            }
+        })
     }
 })
 
-Telegram.on('inline_query', query=>{
+Telegram.on('inline_query', query => {
     //console.log(query);
     let results = []
-    let img = png(query.query, imageSettings)
+    let img = [png(query.query, imageSettings[0]), png(query.query, imageSettings[1])]
     results.push({
-        id: '1',
+        id: 'black',
         type: 'photo',
         title: 'Black',
         caption: query.query,
-        photo_url: '',
-        thumb_url: ''
-    })
+        photo_file_id: 'resources/black.png',
+        //thumb: './resources/black.png'
+   })
+  /* results.push({
+        id: 'white',
+        type: 'photo',
+        title: 'White',
+        caption: query.query,
+        //photo_url: './resources/white.png',
+        //thumb_url: './resources/white.png'
+    })*/
     Telegram.answerInlineQuery(query.id, results)
 })
